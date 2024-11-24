2eb7c1b9d77a9004b1d7cb28a36277514cf57e42
FABI0FERREIRA <fabferoli@gmail.com>
2024-11-23 23:24:20 -0300

    first_commit1

--- services.sh
+++ services.sh
@@ -1,3 +1,4 @@
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
+gcloud config set project pure-toolbox-442517-u4
